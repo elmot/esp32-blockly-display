8c98fbbd15cd931b8a4fba25045882a0a75321a5
Ilia (Elias) Motornyi <elijah.mot@gmail.com>
2024-05-03 22:35:56 +0300

    Digits, rus letters, symbols

--- html/led_control.js
+++ html/led_control.js
@@ -6,7 +6,7 @@ function segm16Output(pos, segments) {
     for (let i = 0; i < 17; i++) {
         const segm = svg.getSVGDocument().querySelectorAll("g g")[i].querySelector("circle, polygon")
         const c = String.fromCharCode(i + 65)
-        const color = segments.indexOf(c) >= 0 ? activeColor : inactiveColor
+        const color = segments.toUpperCase().indexOf(c) >= 0 ? activeColor : inactiveColor
         segm.style.fill = color
     }
 }
@@ -40,8 +40,62 @@ const font = {
     'X': 'IKMO',
     'Y': 'BEJNP',
     'Z': 'CDGHIM',
+    '0': 'ABCDEFGH',
+    '1': 'EF',
+    '2': 'ACDEGHJN',
+    '3': 'CDEFGHNJ',
+    '4': 'BEFJN',
+    '5': 'BCDFGHJN',
+    '6': 'ABCDFGHJN',
+    '7': 'CDEF',
+    '8': 'ABCDEFGHJN',
+    '9': 'BCDEFGHJN',
+    '+': 'JLNP',
+    '-': 'JN',
+    '_': 'GH',
+    '[': 'ABCH',
+    ']': 'DEFG',
+    '>': 'IK',
+    '<': 'MO',
+    '|': 'LP',
+    '/': 'IM',
+    '\\': 'KO',
+    '*': 'IJKLMNOP',
+    '$': 'BCDFGHJLNP',
+    'Й': 'ABDEFIM',
+    'Ц': 'ABEFGHO',
+    'У': 'IKM',
+    'К': 'ABJMO',
+    'Е': 'ABCDGHJ',
+    'Н': 'ABEFJN',
+    'Г': 'ABCD',
+    'Ш': 'ABEFGHLP',
+    'Щ': 'BEFJLN',
+    'З': 'CDEFGHJN',
+    'Х': 'IKMO',
+    'Ъ': 'CFGLNP',
+    'Ф': 'BCDEJLNP',
+    'Ы': 'ABEFHJP',
+    'В': 'ABCDEFGHJN',
+    'А': 'ABCDEFJN',
+    'П': 'ABCDEF',
+    'Р': 'ABCDEJN',
+    'О': 'ABCDEFGH',
+    'Л': 'EFIM',
+    'Д': 'EFGHIM',
+    'Ж': 'IJKLMNOP',
+    'Э': 'IJK',
+    'Я': 'BCDEFIJN',
+    'Ч': 'BEFJN',
+    'С': 'ABCDGH',
+    'М': 'ABEFKM',
+    'И': 'ABEFIM',
+    'Т': 'CDLP',
+    'Ь': 'ABFGHJN',
+    'Б': 'ABCDFGHJN',
+    'Ю': 'ABDEFGJLP',
 };
-
+М
 // noinspection JSUnusedGlobalSymbols
 function segm16Text(text) {
     for (let i = 0; i < 4; i++) {
